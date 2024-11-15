77e422adb84e2bdb2aa3c0c688f365f29ea8e8fc
almirashynybek <156778698+almirashynybek@users.noreply.github.com>
2024-11-15 15:19:50 +0500

    Update 5.py

--- hw_9/5.py
+++ hw_9/5.py
@@ -1,16 +1,17 @@
 # Задание 3: Конвертер температур
 
 class TemperatureConverter:
-
-    def celsius_to_fahrenheit(self, celsius):
+    
+    @staticmethod
+    def celsius_to_fahrenheit( celsius):
         return ((celsius * 9//5) + 32)
     
-    def fahrenheit_to_celsius(self, fahrenheit):
+    @staticmethod
+    def fahrenheit_to_celsius( fahrenheit):
         return ((32 - fahrenheit) * 5//9)
     
 
 result = TemperatureConverter()
 
 print(result.celsius_to_fahrenheit(5))
-
 print(result.fahrenheit_to_celsius(20))
