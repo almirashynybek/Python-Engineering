4f4e92d1c30b2b1d94ce389775a4bdab5395e1ba
almirashynybek <almirashynybek@gmail.com>
2024-11-26 21:43:57 +0500

    Миксины.Пример из урока

--- hw_11/1.py
+++ hw_11/1.py
@@ -1,23 +1,58 @@
-class Vehicle:
-    def __init__(self, speed):
-        self.speed = speed
+import time
+
+class LoggingMixin:
+    def log(self, message:str):
+        print(f"[LOG] {message}")
+
+
+class CachMixin:
+    cach: dict = {}
+
+    def get(self, key:int):
+        if key in self.cach:
+            return self.cach[key]
+        
+    def set(self, key:int, value:dict):
+        self.cach[key] = value
+
+
+class ProductService(CachMixin, LoggingMixin):
+    def get_product(self, product_id: int):
+        product = self.get(product_id)
+
+        if product is None:
+            self.log(f'Product {product_id} not found in cache')
+
+            product = self.get_product_from_db(product_id)
+            self.set(product_id, product)
+            self.log(f'Product {product_id} is added to cache')
+
+        else:
+            return product
+        
     
-    def move(self):
-        print(f"Vehicle moves with speed {self.speed} km/h ")
+    def get_product_from_db(self, product_id:int):
+        self.log('Обращаюсь к БД. Будет задержка в 3 секунды')
 
+        time.sleep(3)
+        self.log(f'Product {product_id} loaded from DB')
+        return {"id": product_id, "name": f"Product {product_id}"}
     
-class Car(Vehicle):
-    def move(self):
-        print(f"Car moves with speed {self.speed} km/h ")
 
+product_service = ProductService()
+product_id_1 = 1
+product_id_2 = 2
 
-class Bicycle(Vehicle):
-    def move (self):
-        print(f"Bicycle moves with speed {self.speed} km/h ")
+# Первый вызов: обращение к базе данных (так как данных в кэше еще нет)
+product_1_first_call = product_service.get_product(product_id_1)
+product_1_second_call =product_service.get_product(product_id_1) # Второй вызов из кэша
+product_1_third_call = product_service.get_product(product_id_1) # Третий вызов из кэша
 
+print(product_1_first_call)
+print(product_1_second_call)
+print(product_1_third_call)
 
-car = Car(25)   
-bicycle = Bicycle(15)
-
-car.move()
-bicycle.move()
+# Тот же процесс для другого продукта
+product_2_first_call = product_service.get_product(product_id_2)
+product_2_second_call = product_service.get_product(product_id_2)
+product_2_third_call =product_service.get_product(product_id_2)
